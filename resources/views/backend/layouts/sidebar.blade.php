916a0946de1cb0657998f302c0f87a3469064c6b
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-02-25 16:17:06 +0600

    finished backend design

---
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -0,0 +1,24 @@
+
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the -->
+
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-copy"></i>
+                <p>
+                    User Management
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="pages/layout/top-nav.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>View Users</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</nav>
