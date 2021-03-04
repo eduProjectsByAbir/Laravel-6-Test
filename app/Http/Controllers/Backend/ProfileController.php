c4fcad5c16acb1fd770e26ec0448c0cbcc512f07
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-03-04 17:31:03 +0600

    date0403

--- app/Http/Controllers/Backend/ProfileController.php
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
+use App\User;
 
 class ProfileController extends Controller
 {
     public function view(){
-        return view('backend.user.view-profile');
+        $id = Auth::user()->id;
+        $user = User::find($id);
+        return view('backend.user.view-profile', compact('user'));
     }
 }
--- resources/views/backend/user/view-profile.blade.php
+++ resources/views/backend/user/view-profile.blade.php
@@ -25,17 +25,36 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <section class="col-md-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3>Manage Profile
-                                    <a class="btn btn-success float-right" href="{{route('users.add')}}"><i class="fa fa-plus-circle"></i> Add User</a>
-                                </h3>
+                    <section class="col-md-4 offset-md-4">
+                            <div class="card card-primary card-outline">
+                                <div class="card-body box-profile">
+                                    <div class="text-center">
+                                        <img class="profile-user-img img-fluid img-circle"
+                                             src="{{asset('public/backend/upload/no-image.png')}}"
+                                             alt="User profile picture">
+                                    </div>
+
+                                    <h3 class="profile-username text-center">{{ $user->name  }}</h3>
+
+                                    <p class="text-muted text-center">Software Engineer</p>
+
+                                    <ul class="list-group list-group-unbordered mb-3">
+                                        <li class="list-group-item">
+                                            <b>Followers</b> <a class="float-right">1,322</a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <b>Following</b> <a class="float-right">543</a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <b>Friends</b> <a class="float-right">13,287</a>
+                                        </li>
+                                    </ul>
+
+                                    <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+                                </div>
+                                <!-- /.card-body -->
                             </div>
-                            <div class="card-body">
-                                hello
-                            </div>
-                        </div>
+                            <!-- /.card -->
                     </section>
                 </div>
                 <!-- /.row -->
